490dd6ed888a6498823109418c0ba9cf10a564ed
Diogo Medeiros <diogo_medeiros@sapo.pt>
2024-12-03 23:24:45 +0000

    Day 1 & 2

---
+++ AdventOfCode/SharedLib/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace SharedLib;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<(T First, T Second)> Pairwise<T>(this IEnumerable<T> source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        using var enumerator = source.GetEnumerator();
+        if (!enumerator.MoveNext()) yield break;
+
+        var previous = enumerator.Current;
+        while (enumerator.MoveNext())
+        {
+            var current = enumerator.Current;
+            yield return (previous, current);
+            previous = current;
+        }
+    }
+
+    public static IEnumerable<T> SkipAt<T>(this IEnumerable<T> source, int index)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var i = 0;
+        foreach (var item in source)
+        {
+            if (i++ == index) continue;
+            yield return item;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Advent-of-Code-2024
+
+Source code for the [2024 edition of Advent of Code](https://adventofcode.com/2024).
+
+The solutions are written in C# 13 (.NET 9). To run a solution, use the following command:
+
+```bash
+dotnet run DayX/.
+```
+
+## Progress
+
+| Day | Part One | Part Two |
+|----:|:--------:|:--------:|
+|   1 |    ⭐     |    ⭐     |
+|   2 |    ⭐     |    ⭐     |
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
